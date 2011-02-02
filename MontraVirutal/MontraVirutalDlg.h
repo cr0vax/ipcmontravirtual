882c780367f398103d5e5c3fc28c6b696914dc70
bruno.as.moreira <bruno.as.moreira@gmail.com>
2011-02-02 00:01:15 +0000

    - rotação de imagens automática  - diferenciação de timers  - imagens desfocadas

--- MontraVirutal/MontraVirutalDlg.h
+++ MontraVirutal/MontraVirutalDlg.h
@@ -4,6 +4,7 @@
 
 #pragma once
 #include "afxwin.h"
+#include "PictureEx.h"
 
 
 // CMontraVirutalDlg dialog
@@ -29,15 +30,17 @@ protected:
 	afx_msg void OnPaint();
 	afx_msg HCURSOR OnQueryDragIcon();
 	DECLARE_MESSAGE_MAP()
-public:
-//	afx_msg void OnStnClickedCenterImage();
-//	CStatic m_images;
 
 public:
 
 	afx_msg void OnBnClickedButtonLeft();
 	afx_msg void OnBnClickedButtonRight();
-	
+	afx_msg void OnTimer(UINT_PTR nIDEvent);
+	void rotateLeft();
+	void rotateRight();
+	void hideHands(int iShow);
+	void timer(int iType);
+
 	CStatic m_left;
 	CStatic m_center;
 	CStatic m_right;
@@ -46,8 +49,11 @@ public:
 	CStatic m_description_2;
 	CStatic m_description_3;
 
+	// array de imagens
 	int m_images[5];
+	int m_images_b[5];
 
+	// arrays de descrições
 	CString m_descrptions_1[5];
 	CString m_descrptions_2[5];
 	CString m_descrptions_3[5];
@@ -55,4 +61,10 @@ public:
 	int m_left_id;
 	int m_center_id;
 	int m_right_id;
+
+	int m_timer;
+
+	CPictureEx m_left_hand;
+	CPictureEx m_right_hand;	
+	CPictureEx m_left_hand2;
 };
